5225233b5b4dd101b476756f6b66d20056ebc84f
Oladayo Olukowi <oladayoolukowi@gmail.com>
2019-05-23 16:41:25 +0100

    Create week6ass.js

---
+++ week6ass.js
@@ -0,0 +1,397 @@
+const nameOfWorkers=[
+{
+    Firstname: 'Vanessa',
+    Lastname: 'Rogers',
+    Gender: 'Female',
+    Age: 19,
+    Salary: 60193
+},
+{
+    Firstname: 'Charlotte',
+    Lastname: 'Hunt',
+    Gender: 'Female',
+    Age: 24,
+    Salary: 92383
+},
+{
+    Firstname: 'Marcus',
+    Lastname: 'Foster',
+    Gender: 'Male',
+    Age: 21,
+    Salary: 65760
+},
+{
+    Firstname: 'Myra',
+    Lastname: 'Brooks',
+    Gender: 'Female',
+    Age: 21,
+    Salary: 174036
+},
+{
+    Firstname: 'Caroline',
+    Lastname: 'Armstrong',
+    Gender: 'Female',
+    Age: 29,
+    Salary: 149879
+},
+{
+    Firstname: 'Belinda',
+    Lastname: 'Carroll',
+    Gender: 'Female',
+    Age: 19,
+    Salary: 188935
+},
+{
+    Firstname: 'Olivia',
+    Lastname: 'Armstrong',
+    Gender: 'Female',
+    Age: 26,
+    Salary: 34782
+},
+{
+    Firstname: 'April',
+    Lastname: 'Cooper',
+    Gender: 'Female',
+    Age: 20,
+    Salary: 68263
+},
+{
+    Firstname: 'Edith',
+    Lastname: 'Harrison',
+    Gender: 'Female',
+    Age: 23,
+    Salary: 54709
+},
+{
+    Firstname: 'Vivian',
+    Lastname: 'Henderson',
+    Gender: 'Female',
+    Age: 20,
+    Salary: 155709
+},
+{
+    Firstname: 'Ashton',
+    Lastname: 'Martin',
+    Gender: 'Male',
+    Age: 27,
+    Salary: 166647
+},
+{
+    Firstname: 'Gianna',
+    Lastname: 'Moore',
+    Gender: 'Female',
+    Age: 23,
+    Salary: 191619
+},
+{
+    Firstname: 'Camila',
+    Lastname: 'Allen',
+    Gender: 'Female',
+    Age: 23,
+    Salary: 85567
+},
+{
+    Firstname: 'Gianna',
+    Lastname: 'Morrison',
+    Gender: 'Female',
+    Age: 28,
+    Salary: 191509
+},
+{
+    Firstname: 'Paul',
+    Lastname: 'Cooper',
+    Gender: 'Male',
+    Age: 28,
+    Salary: 39471
+},
+{
+    Firstname: 'Tara',
+    Lastname: 'Mason',
+    Gender: 'Female',
+    Age: 19,
+    Salary: 189780
+},
+{
+    Firstname: 'Aida',
+    Lastname: 'Tucker',
+    Gender: 'Female',
+    Age: 21,
+    Salary: 143187
+},
+{
+    Firstname: 'Richard',
+    Lastname: 'Cameron',
+    Gender: 'Male',
+    Age: 21,
+    Salary: 116644
+},
+{
+    Firstname: 'Amy',
+    Lastname: 'Stewart',
+    Gender: 'Female',
+    Age: 29,
+    Salary: 126961
+},
+{
+    Firstname: 'Eleanor',
+    Lastname: 'Thompson',
+    Gender: 'Female',
+    Age: 22,
+    Salary: 159003
+},
+{
+    Firstname: 'Alberta',
+    Lastname: 'Edwards',
+    Gender: 'Female',
+    Age: 21,
+    Salary: 105648
+},
+{
+    Firstname: 'Gianna',
+    Lastname: 'Evans',
+    Gender: 'Female',
+    Age: 27,
+    Salary: 183805
+},
+{
+    Firstname: 'Florrie',
+    Lastname: 'Kelley',
+    Gender: 'Female',
+    Age: 24,
+    Salary: 180568
+},
+{
+    Firstname: 'Miranda',
+    Lastname: 'Harris',
+    Gender: 'Female',
+    Age: 19,
+    Salary: 185753
+},
+{
+    Firstname: 'Henry',
+    Lastname: 'Davis',
+    Gender: 'Male',
+    Age: 18,
+    Salary: 83804
+},
+{
+    Firstname: 'Kirsten',
+    Lastname: 'Barnes',
+    Gender: 'Female',
+    Age: 26,
+    Salary: 161833
+},
+{
+    Firstname: 'Jack',
+    Lastname: 'Chapman',
+    Gender: 'Male',
+    Age: 19,
+    Salary: 41007
+},
+{
+    Firstname: 'Myra',
+    Lastname: 'Kelly',
+    Gender: 'Female',
+    Age: 27,
+    Salary: 118346
+},
+{
+    Firstname: 'Emma',
+    Lastname: 'Evans',
+    Gender: 'Female',
+    Age: 23,
+    Salary: 126125
+},
+{
+    Firstname: 'Blake',
+    Lastname: 'Evans',
+    Gender: 'Male',
+    Age: 24,
+    Salary: 69702
+},
+{
+    Firstname: 'Aldus',
+    Lastname: 'Armstrong',
+    Gender: 'Male',
+    Age: 26,
+    Salary: 132491
+},
+{
+    Firstname: 'Adelaide',
+    Lastname: 'Martin',
+    Gender: 'Female',
+    Age: 20,
+    Salary: 175263
+},
+{
+    Firstname: 'Victoria',
+    Lastname: 'Elliott',
+    Gender: 'Female',
+    Age: 21,
+    Salary: 111645
+},
+{
+    Firstname: 'Nicole',
+    Lastname: 'Harrison',
+    Gender: 'Female',
+    Age: 22,
+    Salary: 54624
+},
+{
+    Firstname: 'Edwin',
+    Lastname: 'Hamilton',
+    Gender: 'Male',
+    Age: 18,
+    Salary: 152586
+},
+{
+    Firstname: 'Jessica',
+    Lastname: 'Robinson',
+    Gender: 'Female',
+    Age: 29,
+    Salary: 85446
+},
+{
+    Firstname: 'Sofia',
+    Lastname: 'Turner',
+    Gender: 'Female',
+    Age: 20,
+    Salary: 131564
+},
+{
+    Firstname: 'Cadie',
+    Lastname: 'Higgins',
+    Gender: 'Female',
+    Age: 21,
+    Salary: 138663
+},
+{
+    Firstname: 'Jessica',
+    Lastname: 'Turner',
+    Gender: 'Female',
+    Age: 24,
+    Salary: 91319
+},
+{
+    Firstname: 'Walter',
+    Lastname: 'Hamilton',
+    Gender: 'Male',
+    Age: 25,
+    Salary: 117408
+},
+{
+    Firstname: 'Alexander',
+    Lastname: 'Farrell',
+    Gender: 'Male',
+    Age: 26,
+    Salary: 65878
+},
+{
+    Firstname: 'Justin',
+    Lastname: 'Howard',
+    Gender: 'Male',
+    Age: 21,
+    Salary: 165575
+},
+{
+    Firstname: 'Chelsea',
+    Lastname: 'Holmes',
+    Gender: 'Female',
+    Age: 21,
+    Salary: 32690
+},
+{
+    Firstname: 'Alberta',
+    Lastname: 'Wilson',
+    Gender: 'Female',
+    Age: 22,
+    Salary: 143459
+},
+{
+    Firstname: 'Miller',
+    Lastname: 'Murphy',
+    Gender: 'Male',
+    Age: 25,
+    Salary: 163776
+},
+{
+    Firstname: 'Adrianna',
+    Lastname: 'Hawkins',
+    Gender: 'Female',
+    Age: 29,
+    Salary: 64412
+},
+{
+    Firstname: 'Dominik',
+    Lastname: 'Reed',
+    Gender: 'Male',
+    Age: 23,
+    Salary: 42643
+},
+{
+    Firstname: 'Gianna',
+    Lastname: 'Craig',
+    Gender: 'Female',
+    Age: 18,
+    Salary: 141944
+},
+{
+    Firstname: 'Alissa',
+    Lastname: 'Cameron',
+    Gender: 'Female',
+    Age: 25,
+    Salary: 101581
+},
+{
+    Firstname: 'Aldus',
+    Lastname: 'Adams',
+    Gender: 'Male',
+    Age: 28,
+    Salary: 132894
+}
+];
+
+// Firstname and Lastname using forEach
+    nameOfWorkers.forEach(names  => console.log(` ${names.Firstname} ${names.Lastname}`));
+   
+//Total salaries of workers using reduce
+   const sumOfSalaries = nameOfWorkers.reduce((acc, curr) => acc + curr.Salary, 0);
+   console.log (sumOfSalaries);
+
+// Average age of workers
+    const sumOfAges = nameOfWorkers.reduce((acc, curr) => acc + curr.Age, 0);
+    console.log (sumOfAges / nameOfWorkers.length);
+// or
+    const averageAge = sumOfAges/nameOfWorkers.length;
+    console.log (averageAge);
+
+// Workers above 20 years
+    const workersabove20 = nameOfWorkers.filter(above20 => above20.Age > 20);
+    console.log (workersabove20)    
+
+// New salaries of workers 
+     nameOfWorkers.map(newSalary => newSalary.Salary * 2).forEach(newSalary => console.log (newSalary));
+
+// Function that takes in an array of number as a parameter
+    let array = [1, 2, 3, 4, 5, 6, 7];
+    const firstAndLast = (array) => { 
+        return `${array[0]} ${array.length}`
+    };
+    console.log (firstAndLast(array));
+
+
+//Function that accepts object as parameter
+let obj = {
+     Firstname: 'Jack',
+    Lastname: 'Chapman',
+    Gender: 'Male',
+    Age: 19,
+    Salary: 41007
+};
+const newObj = (obj) => `Your name is ${obj.Firstname} ${obj.Lastname}, you are a ${obj.Gender} and your current salary is $${obj.Salary}`;
+console.log (newObj(obj));
+
+
+//Creating a one-line function
+const barca = (name, score) => `Hello ${name}, you scored ${score} goals today`;
+console.log (barca ('Lionel Messi', 3));
